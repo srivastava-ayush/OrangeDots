f69f123ab23154e09b2c12e1817bd9ca5bfcd93f
Ayush Srivastava <constayush@gmail.com>
2026-09-10 17:18:49 +0530

    updated , added liquid glass

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -423,3 +423,46 @@ plugin = {
         blur             = false,
     },
 }
+
+
+if hl.plugin.hyprglass then
+    local hg = hl.plugin.hyprglass
+
+    hg.config({
+        enabled = true,
+        default_theme = "dark",
+        default_preset = "glass",
+        layers = { enabled = true },
+    })
+
+    hg.preset("apple", {
+        blur_strength        = 2.2,
+        blur_iterations      = 3,
+        refraction_strength  = 0.55,
+        chromatic_aberration = 0.3,
+        fresnel_strength     = 0.5,
+        specular_strength    = 0.75,
+        edge_thickness       = 0.05,
+        lens_distortion      = 0.3,
+
+        dark = {
+            brightness   = 0.82,
+            contrast     = 0.90,
+            saturation   = 0.80,
+            vibrancy     = 0.15,
+            adaptive_dim = 0.4
+        },
+
+        light = {
+            brightness     = 1.12,
+            contrast       = 0.92,
+            saturation     = 0.85,
+            vibrancy      = 0.12,
+            adaptive_boost = 0.4
+        }
+    })
+    hg.layer("caelestia-background", { exclude = true })
+    hg.layer("caelestia-border-exclusion", { exclude = true })
+    hg.layer("quickshell", { exclude = true })
+    hg.layer("caelestia-drawers", { exclude = true })
+end
